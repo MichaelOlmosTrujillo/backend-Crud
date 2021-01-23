2825412e873a5e26aea80ef680bccab8f4e158c7
Michael Olmos Trujillo <molmost@unal.edu.co>
2021-01-22 19:06:35 -0500

    creando el backend y las variables de entorno

---
+++ app.js
@@ -0,0 +1,18 @@
+// Express construye el backend
+const express = require('express');
+// Llamado de express
+const app = express();
+// Cors permite comunicar el frontend con el backend, permite hacer apis
+const cors = require('cors');
+// Permite cargar las variables del entorno en un archivo .env
+const dotenv = require('dotenv');
+// Permite usar dotenv
+dotenv.config();
+// permite usar cors
+app.use(cors());
+// Permite enviar los datos entre el backend y el frontend en formato json
+app.use(express.json());
+// No permite enviar formularios
+app.use(express.urlencoded({extended:false}));
+
+
