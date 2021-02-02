be417fa493b50350c38ee656fc05206e7cfdfee0
Michael Olmos Trujillo <molmost@unal.edu.co>
2021-02-02 01:56:01 -0500

    Delete function is finished and creating edit function

--- app.js
+++ app.js
@@ -48,6 +48,11 @@ app.get("/getAll", (request, response) => {
 // Delete
 app.delete('/delete/:id', (request, response)=>{
   const {id} = request.params;
+  const db = dbService.getDbServiceInstance();
+  const result = db.deleteRowById(id);
+  result
+  .then(data => response.json({success:data}))
+  .catch(err => console.log(err));
 })
 
 app.listen(process.env.PORT, () => console.log("app is running"));
--- dbService.js
+++ dbService.js
@@ -81,12 +81,13 @@ class DbService {
 
         connection.query(query, [id], (err, result)=>{
           if(err) reject(new Error(err.message));
-          resolve(result);
+          resolve(result.affectedRows);
         })
       })
-      console.log(response);
+      return response === 1 ? true: false;
     }catch(error){
       console.log(error);
+      return false;
     }
   }
 }
